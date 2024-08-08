99464b44a9181a510d6d4c936a74a7b20e024d76
Arshree <98222843+Arshree@users.noreply.github.com>
2024-08-08 18:21:10 +0530

    small changes

--- cypress/e2e/coding/CSSLocators.cy.js
+++ cypress/e2e/coding/CSSLocators.cy.js
@@ -1,10 +1,11 @@
 
+import {ALL_DATA} from '../supports/selectors';
 
 describe('CSSLocator', () =>
 {
     it("csslocators", () =>
     {
-        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.visit(ALL_DATA.URL2)
         cy.get("input[name='username']").type("Admin")
 
         cy.get("input[name='password']").type("admin123")
